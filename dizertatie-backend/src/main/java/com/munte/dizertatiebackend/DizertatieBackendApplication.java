3654ed0577100c344b90c0a1b49706ae6be3ba16
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2022-10-25 12:55:41 +0300

    Inceput

---
+++ dizertatie-backend/src/main/java/com/munte/dizertatiebackend/DizertatieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.munte.dizertatiebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DizertatieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DizertatieBackendApplication.class, args);
+	}
+
+}
---
+++ dizertatie-backend/src/test/java/com/munte/dizertatiebackend/DizertatieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munte.dizertatiebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DizertatieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
