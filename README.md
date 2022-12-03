98a7f79bcc7e1c5ed374be3019652e6d3fae1726
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2022-12-03 18:00:05 +0200

    front: added details to the food list

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ RESTful API &amp; Angular client
 
 - java 17 
 - node.vs v16.17.1
+
+- Front:
+-> trebuie sa fac cumva sa apara stelele la mancare
--- disertatie-frontend/src/app/services/food/food.service.ts
+++ disertatie-frontend/src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Food } from 'src/app/shared/models/Food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,74 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():String[] {
+  getAll():Food[] {
     return [
-      "./assets/pizza_all_cheese.png",
-      "./assets/pizza_argentina.png",
-      "./assets/pizza_carnivor.png",
-      "./assets/pizza_clasic.png",
-      "./assets/pizza_double_pepperoni.png",
-      "./assets/pizza_rustic.png"
-    ]
+      {
+        id: 1,
+        name: 'Pizza All Cheese',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.5,
+        imageUrl: "./assets/pizza_all_cheese.png",
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 2,
+        name: 'Pizza Argentina',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: './assets/pizza_argentina.png',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 3,
+        name: 'Pizza Carnivor',
+        price: 5,
+        cookTime: '10-15',
+        favorite: false,
+        origins: ['germany', 'us'],
+        stars: 3.5,
+        imageUrl: './assets/pizza_carnivor.png',
+        tags: ['FastFood', 'Hamburger'],
+      },
+      {
+        id: 4,
+        name: 'Pizza Clasic',
+        price: 2,
+        cookTime: '15-20',
+        favorite: true,
+        origins: ['belgium', 'france'],
+        stars: 3.3,
+        imageUrl: './assets/pizza_clasic.png',
+        tags: ['FastFood', 'Fry'],
+      },
+      {
+        id: 5,
+        name: 'Pizza Double Pepperoni',
+        price: 11,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['india', 'asia'],
+        stars: 3.0,
+        imageUrl: './assets/pizza_double_pepperoni.png',
+        tags: ['SlowFood', 'Soup'],
+      },
+      {
+        id: 6,
+        name: 'Pizza Rustic',
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.0,
+        imageUrl: './assets/pizza_rustic.png',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+    ];
   }
 }
