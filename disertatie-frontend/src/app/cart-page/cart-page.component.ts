d626a9bd9f0256ae3728f1bcffb5698efc821e19
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-05-19 14:22:06 +0300

    added address form into the cart page

--- disertatie-frontend/src/app/cart-page/cart-page.component.ts
+++ disertatie-frontend/src/app/cart-page/cart-page.component.ts
@@ -12,6 +12,17 @@ export class CartPageComponent implements OnInit {
 
   cart!: Cart;
 
+  name: String;
+  mail: String;
+  phoneNumber: String;
+  country: String;
+  city: String;
+  street: String;
+  streetNumber: Number;
+  building: String;
+  room: Number;
+
+
   constructor(private cartService: CartService) {
     this.setCart();
   }
@@ -32,6 +43,18 @@ export class CartPageComponent implements OnInit {
     const quantity = parseInt(quantityInString);
 
     this.cartService.changeQuantity(cartItem.food.name, quantity);
+  }
+
+  order() {
+    console.log(this.name);
+    console.log(this.mail);
+    console.log("0" + this.phoneNumber);
+    console.log(this.country);
+    console.log(this.city);
+    console.log(this.street);
+    console.log(this.streetNumber);
+    console.log(this.building);
+    console.log(this.room);
   }
 
 }
